16761bac70cbebf2096a6c10feabf101b0d0d9e6
fatma gülfidan <nesepalamutlari@icloud.com>
2025-01-20 09:46:34 +0100

    first commit

---
+++ school-bikes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+
+
+class Program
+{
+static void Main (string[] args)
+{
+
+    AnsiConsole.Write(
+    new FigletText("Bike Sales")
+     .LeftJustified()
+     .Color(Color.Red));
+
+    AnsiConsole.Write(
+        new BarChart()
+        .Width(60)
+        .CenterLabel()
+        .AddItem("Mountain-Bikes",50,Color.Yellow)
+         .AddItem("E-Bikes",54,Color.Green)
+          .AddItem("Velos",12,Color.Red)
+
+    );
+    
+}
+}
